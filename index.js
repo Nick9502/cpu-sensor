609bc08c7c6a22bea8e7bc028cb52385746c6dfd
Nick The Dev <nickthedev95@gmail.com>
2018-10-31 12:51:01 -0400

    Begun Cross Platform Module Development

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-/* CPU Sensor Module for Raspberry Pi. 
- * Outputs CPU Load Percentage Every Second.
- */
 "use strict";
 const os = require("os"); // Load OS module.
 const delaySeconds = 1;
 const decimals = 3;
 
-
+/* loop() necessary to invoke if IIFE not present
 function loop() {
 	console.log("CPU load is " + cpuLoad() );
 	setTimeout(loop, delaySeconds * 1000);
 }
+* / 
 
+/* Immeditately Invoked Function Expression (IIFE) */
+(function loop() {
+	console.log('CPU load is ' + cpuLoad());
+	setTimeout(loop, delaySeconds * 1000);
+  })();
+
+/* Retreive CPU performance as percentage */
 function cpuLoad() {
 	let cpuLoad = os.loadavg()[0] * 100;
 	return cpuLoad.toFixed(decimals);
 }
 
-loop();
 
